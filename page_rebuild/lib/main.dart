34a6e53fa718cb8513e05da35f9acbac4c24ed28
Brian Kan <brian@endevstudios.com>
2020-09-02 02:01:33 -0400

    Gave specific fonts to the various texts.  Also added box shadows to Analysis and Summary sections.  Lastly, updated the Profile button with color and border radius.

--- page_rebuild/lib/main.dart
+++ page_rebuild/lib/main.dart
@@ -3,71 +3,132 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  void answerQuestion() {
-    print("Answer chosen!");
-  }
-
   Widget build(BuildContext context) {
-    var questions = [
-      "What's your favourite colour?",
-      "What's your favourite animal?",
-    ];
     return MaterialApp(
       home: Scaffold(
-        // appBar: AppBar(
-        //   title: Text(questions[0]),
-        // ),
         body: Column(
           children: <Widget>[
             Container(
-              // color: Colors.blue,
-              // ../resources/headerBacgkround.jpg
+              height: 200,
               decoration: BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage("./../resources/headerBackground.jpg"),
+                  image: AssetImage(
+                    "../page_rebuild/resources/headerBackground.jpg",
+                  ),
                   fit: BoxFit.cover,
                 ),
               ),
-              height: 200,
             ),
             Container(
-              color: Colors.green,
-              child: Text("Diagnosis"),
-              width: double.infinity,
-            ),
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.teal[100],
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Diagnosis",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                )),
             Container(
+              padding: EdgeInsets.all(15),
               color: Colors.white,
               child: Column(
                 children: <Widget>[
                   Column(
                     children: [
                       Text(
-                          "If you would like to be connected to a physician, please give us the following information (for billing purposes)"),
+                        "If you would like to be connected to a physician, please give us the following information (for billing purposes)",
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.lightBlue[900],
+                        ),
+                      ),
                       RaisedButton(
                         child: Text("Update Profile"),
-                        onPressed: null,
+                        onPressed: () {},
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        color: Colors.teal[200],
                       ),
                       Text(
-                          "Hi there, you might want to seek professional medical opinion."),
+                        "Hi there, you might want to seek professional medical opinion.",
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.lightBlue[900],
+                        ),
+                      ),
                     ],
                   ),
                   Column(
                     children: <Widget>[
-                      Text("Analysis Results"),
+                      Text(
+                        "Analysis Results",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.lightBlue[900],
+                        ),
+                      ),
                       Container(
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black,
+                              blurRadius: 20.0,
+                            ),
+                          ],
+                        ),
                         child: Column(
                           children: <Widget>[
                             Text("<Insert Blue Bar Graphic>"),
-                            Text("Melanocytic Nevi 100.0%"),
+                            Text(
+                              "Melanocytic Nevi 100.0%",
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.lightBlue[900],
+                              ),
+                            ),
                           ],
                         ),
                       ),
-                      Text("Summary:"),
+                      Text(
+                        "Summary:",
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.lightBlue[900],
+                        ),
+                      ),
                       Container(
                         child: Column(
                           children: <Widget>[
-                            Text("Melanocytic Nevi"),
-                            Text("Melanocytic Nevi: a benign..."),
+                            Text(
+                              "Melanocytic Nevi",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.lightBlue[900],
+                              ),
+                            ),
+                            Text(
+                              "Melanocytic Nevi: a benign...",
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.lightBlue[900],
+                              ),
+                            ),
                           ],
                         ),
                       ),
